2cad4bd83b1820b84f9ba10522a3efbd576d6b4e
carla-ng <carlitashata@gmail.com>
2023-02-25 00:11:28 +0100

    Creation of project.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My Pokémon Project</h1>
+    <p>Here you can find some categories to choose from to learn more about the world of Pokémon!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
